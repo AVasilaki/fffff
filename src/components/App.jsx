dfd302f6915927e4d0b9486056f4c99e5e941ff2
AVasilaki <poseydon463@gmail.com>
2023-10-05 13:33:17 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,6 @@ export const App = () => (
 );
 
 //! Або можна зробити ось так без import styles
-
-// export const App = () => (
-//   <>
-//     <h1 className="text-yellow-300 font-extrabold font-sans text-6xl">Hello, React</h1>
-//   </>
-// );
-
 //! Моя вам порада - оскільки ми використовуємо Tailwind,
 //! ми повністю позбавляємося CSS файлів, тому краще писати інлайн стилі,
 //! оскільки Tailwind сам все зробить за нас, а також неперевершено оптимізує
